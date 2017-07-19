f5735be0f5bdf4fadb3245e4d17326064cec2059
Pradipta Basu <pradiptabasu@users.noreply.github.com>
2017-07-19 23:50:42 +0530

    script to test network connectivity and ssh

---
+++ physicalNetworkTest.sh
@@ -0,0 +1,16 @@
+clear
+clear
+clear
+traceroute dockerlocal
+traceroute manager1
+traceroute manager2
+traceroute worker1
+traceroute worker2
+traceroute worker3
+
+ping -c 5 dockerlocal
+ping -c 5 manager1
+ping -c 5 manager2
+ping -c 5 worker1
+ping -c 5 worker2
+ping -c 5 worker3
---
+++ sshTest.sh
@@ -0,0 +1,6 @@
+ssh root@dockerlocal ls -lah
+ssh root@manager1 ls -lah
+ssh root@manager2 ls -lah
+ssh root@worker1 ls -lah
+ssh root@worker2 ls -lah
+ssh root@worker3 ls -lah
